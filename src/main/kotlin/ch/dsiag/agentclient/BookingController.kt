4149e628b0adf0789e0e0fd30936d370ece49cb7
Matthias Brun <matthias.brun@gmail.com>
2021-06-19 13:07:41 +0200

    Add http client

--- src/main/kotlin/ch/dsiag/agentclient/BookingController.kt
+++ src/main/kotlin/ch/dsiag/agentclient/BookingController.kt
@@ -2,6 +2,8 @@ package ch.dsiag.agentclient
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 import javax.annotation.PostConstruct
@@ -14,13 +16,22 @@ class BookingController(val bookingRepository: BookingRepository) {
     @PostConstruct
     fun init() {
         logger.info("Init booking")
-        bookingRepository.save(Booking(UUID.randomUUID().toString(), "Matthias Brun"))
-        bookingRepository.save(Booking(UUID.randomUUID().toString(), "Romana Rimar"))
+        bookingRepository.save(Booking(uuid(), "Matthias Brun"))
+        bookingRepository.save(Booking(uuid(), "Romana Rimar"))
     }
 
     @GetMapping("bookings")
     fun getBookings(): List<Booking> {
         return bookingRepository.findAll().toList()
     }
+
+    @PostMapping("bookings")
+    fun createBooking(@RequestBody createBooking: CreateBooking): Booking {
+        return bookingRepository.save(Booking(uuid(), createBooking.name))
+    }
+
+    private fun uuid() = UUID.randomUUID().toString()
+
+    data class CreateBooking(val name: String)
 
 }
